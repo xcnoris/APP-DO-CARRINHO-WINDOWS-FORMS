116b6cc42dda56af5ab64845bec20591bc27ac32
Augusto Nascimento <augustobriel12@gmail.com>
2024-05-31 15:41:21 -0300

    Add nome dos TextBox da Frm de pessoas

--- APP DO CARRINHO/Formularios/Pessoas/Frm_Pessoas.Designer.cs
+++ APP DO CARRINHO/Formularios/Pessoas/Frm_Pessoas.Designer.cs
@@ -33,7 +33,7 @@
             this.Btn_Excluir_Pessoas = new System.Windows.Forms.Button();
             this.Btn_Fechar = new System.Windows.Forms.Button();
             this.Lbl_Nome = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Txt_Nome = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -41,16 +41,13 @@
             this.Lbl_Id = new System.Windows.Forms.Label();
             this.Lbl_Cpf = new System.Windows.Forms.Label();
             this.Lbl_Congregacao = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
+            this.Txt_Cidade_Nome = new System.Windows.Forms.TextBox();
+            this.Txt_Congregacao_Nome = new System.Windows.Forms.TextBox();
+            this.Txt_Id = new System.Windows.Forms.TextBox();
+            this.Txt_Cpf = new System.Windows.Forms.TextBox();
             this.Btn_Filtrar = new System.Windows.Forms.Button();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.Txt_Cidade_Cod = new System.Windows.Forms.TextBox();
+            this.Txt_Congregacao_Id = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Btn_Incluir_Pessoas
@@ -104,12 +101,12 @@
             this.Lbl_Nome.TabIndex = 4;
             this.Lbl_Nome.Text = "Nome";
             // 
-            // textBox1
+            // Txt_Nome
             // 
-            this.textBox1.Location = new System.Drawing.Point(177, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(204, 20);
-            this.textBox1.TabIndex = 5;
+            this.Txt_Nome.Location = new System.Drawing.Point(177, 38);
+            this.Txt_Nome.Name = "Txt_Nome";
+            this.Txt_Nome.Size = new System.Drawing.Size(204, 20);
+            this.Txt_Nome.TabIndex = 5;
             // 
             // button1
             // 
@@ -174,49 +171,33 @@
             this.Lbl_Congregacao.TabIndex = 12;
             this.Lbl_Congregacao.Text = "Congregação";
             // 
-            // textBox2
+            // Txt_Cidade_Nome
             // 
-            this.textBox2.Location = new System.Drawing.Point(247, 67);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(134, 20);
-            this.textBox2.TabIndex = 13;
+            this.Txt_Cidade_Nome.Location = new System.Drawing.Point(247, 67);
+            this.Txt_Cidade_Nome.Name = "Txt_Cidade_Nome";
+            this.Txt_Cidade_Nome.Size = new System.Drawing.Size(134, 20);
+            this.Txt_Cidade_Nome.TabIndex = 13;
             // 
-            // textBox3
+            // Txt_Congregacao_Nome
             // 
-            this.textBox3.Location = new System.Drawing.Point(281, 263);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 14;
+            this.Txt_Congregacao_Nome.Location = new System.Drawing.Point(247, 93);
+            this.Txt_Congregacao_Nome.Name = "Txt_Congregacao_Nome";
+            this.Txt_Congregacao_Nome.Size = new System.Drawing.Size(134, 20);
+            this.Txt_Congregacao_Nome.TabIndex = 15;
             // 
-            // textBox4
+            // Txt_Id
             // 
-            this.textBox4.Location = new System.Drawing.Point(247, 93);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(134, 20);
-            this.textBox4.TabIndex = 15;
+            this.Txt_Id.Location = new System.Drawing.Point(461, 38);
+            this.Txt_Id.Name = "Txt_Id";
+            this.Txt_Id.Size = new System.Drawing.Size(172, 20);
+            this.Txt_Id.TabIndex = 16;
             // 
-            // textBox5
+            // Txt_Cpf
             // 
-            this.textBox5.Location = new System.Drawing.Point(461, 38);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(172, 20);
-            this.textBox5.TabIndex = 16;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(461, 67);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(172, 20);
-            this.textBox6.TabIndex = 17;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(444, 208);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 30);
-            this.button4.TabIndex = 18;
-            this.button4.Text = "Fechar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.Txt_Cpf.Location = new System.Drawing.Point(461, 67);
+            this.Txt_Cpf.Name = "Txt_Cpf";
+            this.Txt_Cpf.Size = new System.Drawing.Size(172, 20);
+            this.Txt_Cpf.TabIndex = 17;
             // 
             // Btn_Filtrar
             // 
@@ -227,42 +208,32 @@
             this.Btn_Filtrar.Text = "Filtrar";
             this.Btn_Filtrar.UseVisualStyleBackColor = true;
             // 
-            // textBox7
+            // Txt_Cidade_Cod
             // 
-            this.textBox7.Location = new System.Drawing.Point(179, 67);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(62, 20);
-            this.textBox7.TabIndex = 20;
+            this.Txt_Cidade_Cod.Location = new System.Drawing.Point(179, 67);
+            this.Txt_Cidade_Cod.Name = "Txt_Cidade_Cod";
+            this.Txt_Cidade_Cod.Size = new System.Drawing.Size(62, 20);
+            this.Txt_Cidade_Cod.TabIndex = 20;
             // 
-            // textBox8
+            // Txt_Congregacao_Id
             // 
-            this.textBox8.Location = new System.Drawing.Point(209, 176);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(134, 20);
-            this.textBox8.TabIndex = 21;
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(179, 93);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(62, 20);
-            this.textBox9.TabIndex = 22;
+            this.Txt_Congregacao_Id.Location = new System.Drawing.Point(179, 93);
+            this.Txt_Congregacao_Id.Name = "Txt_Congregacao_Id";
+            this.Txt_Congregacao_Id.Size = new System.Drawing.Size(62, 20);
+            this.Txt_Congregacao_Id.TabIndex = 22;
             // 
             // Frm_Pessoas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.Txt_Congregacao_Id);
+            this.Controls.Add(this.Txt_Cidade_Cod);
             this.Controls.Add(this.Btn_Filtrar);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Txt_Cpf);
+            this.Controls.Add(this.Txt_Id);
+            this.Controls.Add(this.Txt_Congregacao_Nome);
+            this.Controls.Add(this.Txt_Cidade_Nome);
             this.Controls.Add(this.Lbl_Congregacao);
             this.Controls.Add(this.Lbl_Cpf);
             this.Controls.Add(this.Lbl_Id);
@@ -270,7 +241,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Txt_Nome);
             this.Controls.Add(this.Lbl_Nome);
             this.Controls.Add(this.Btn_Fechar);
             this.Controls.Add(this.Btn_Excluir_Pessoas);
@@ -291,7 +262,7 @@
         private System.Windows.Forms.Button Btn_Excluir_Pessoas;
         private System.Windows.Forms.Button Btn_Fechar;
         private System.Windows.Forms.Label Lbl_Nome;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Txt_Nome;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
@@ -299,15 +270,12 @@
         private System.Windows.Forms.Label Lbl_Id;
         private System.Windows.Forms.Label Lbl_Cpf;
         private System.Windows.Forms.Label Lbl_Congregacao;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox Txt_Cidade_Nome;
+        private System.Windows.Forms.TextBox Txt_Congregacao_Nome;
+        private System.Windows.Forms.TextBox Txt_Id;
+        private System.Windows.Forms.TextBox Txt_Cpf;
         private System.Windows.Forms.Button Btn_Filtrar;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox Txt_Cidade_Cod;
+        private System.Windows.Forms.TextBox Txt_Congregacao_Id;
     }
 }
