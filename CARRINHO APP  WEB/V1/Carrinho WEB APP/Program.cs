096614add97c64dee468d2a38897edfe42138219
Augusto Nascimento <augustobriel12@gmail.com>
2024-09-15 19:39:39 -0300

    Add projeto Carrinho Web App

---
+++ CARRINHO APP  WEB/V1/Carrinho WEB APP/Program.cs
@@ -0,0 +1,20 @@
+using Carrinho_WEB_APP;
+using Carrinho_WEB_APP.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
+
+builder.Services.AddTransient<CongregacaoAPI>();
+
+builder.Services.AddHttpClient("API", client => {
+    client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+await builder.Build().RunAsync();
---
+++ CARRINHO APP  WEB/V1/Carrinho WEB APP/Services/CongregacaoAPI.cs
@@ -0,0 +1,23 @@
+﻿using CarrinhoWebApp.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace Carrinho_WEB_APP.Services
+{
+    public class CongregacaoAPI
+    {
+        private readonly HttpClient _httpClient;
+        public CongregacaoAPI(IHttpClientFactory factory)
+        {
+            _httpClient = factory.CreateClient("API");
+        }
+
+
+         // Renomear o método para um nome mais apropriado
+        public async Task<ICollection<CongregacaoModel>?> GetTodasCongregacoesAsync()
+        {
+            // Ajustar o endpoint para a URL correta da API
+            return await _httpClient.GetFromJsonAsync<ICollection<CongregacaoModel>>("api/Congregacao/BuscarTodos");
+        }
+    }
+}
